15c7131f4d5be9b144e6a72b52acdf47989e3517
Henri de Corn <henri.decorn@gmail.com>
2014-04-24 16:43:23 +0200

    Create answers and link with questions

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,5 @@
 class Question < ActiveRecord::Base
-
 	validates :wording, presence: true
+
+	has_many :answers, dependent: :destroy
 end
---
+++ app/views/answers/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @answer, :id, :response, :created_at, :updated_at
